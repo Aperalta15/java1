82c36e00cabc6dcadc19959eb25388ea97ca7856
invitado <invitado@S06-38.cuc.edu.co>
2022-02-15 08:08:39 -0500

    Ejercicio cuena inicio

---
+++ operaciones/src/edu/cuc/pruebas/PruebaCuentas.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.cuc.pruebas;
+
+import cuc.edu.cuenta.Cuenta;
+
+/**
+ *
+ * @author invitado
+ */
+public class PruebaCuentas {
+    public static void main(String[] args) {
+        Cuenta miCuenta = new Cuenta(1, "Cuenta");
+        System.out.println(miCuenta);
+        
+    }
+}
